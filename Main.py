bba4bc9ac3bb9b682d9917073d76d666d3153db5
Dubem-eric <57052749+Dubem-eric@users.noreply.github.com>
2021-03-19 15:44:32 +0100

    Created a simple python script

---
+++ Main.py
@@ -0,0 +1,19 @@
+ This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime
+
+x = datetime.datetime.now()
+
+yr = int(x.strftime("%Y"))
+
+print("What is your name?..")
+
+name = input()
+
+print('What is your date of birth')
+
+date = int(input())
+age = yr-date
+print(" Your name is " + name + " and you are", age)
